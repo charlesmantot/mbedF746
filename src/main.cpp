24a473759d1795db55dc58d15f2565c24aab48e0
Charles Mantot <charles.mantot@gmail.com>
2024-06-04 14:05:42 +0200

    modification des pins et mises en place d'un mutex pour affichage

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,9 @@ ThreadLvgl threadLvgl(30);
 
 int main() {
 
+    const int lightSensor=9;//pin 9 de l'arduino pour capteur
+    const int buzzer=17;
+    float valeur;
     threadLvgl.lock();//mutex a utilise pour les affichages
 
     lv_demo_widgets();//creation des affichages
@@ -17,5 +20,12 @@ int main() {
     while (1) {
         // put your main code here, to run repeatedly:
         ThisThread::sleep_for(10ms);
+        valeur=analogin_read(0);
+        printf("%f",valeur);
     }
 }
+
+//idee :
+//  Pour le clavier numerique utiliser text auto-formatting
+//  Pour affichage heure actu utiliser a clock from a meter
+//  Pour affichage luminosite utiliser text shadow
